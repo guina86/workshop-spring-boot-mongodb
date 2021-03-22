dde34639a7d1790173fbc665c686ad250d2c8cd1
guina86 <guina86@gmail.com>
2021-03-22 15:34:08 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/ginkei/workshopmongo/services/UserService.java
+++ src/main/java/com/ginkei/workshopmongo/services/UserService.java
@@ -37,5 +37,16 @@ public class UserService {
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	
+	public User update(User obj) {
+		var newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 
 }
