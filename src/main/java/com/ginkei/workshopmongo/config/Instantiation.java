242d37cb6a97d02b73b29f3a4e9be52be0f8a612
guina86 <guina86@gmail.com>
2021-03-22 17:57:45 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ginkei/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ginkei/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ginkei.workshopmongo.domain.Post;
 import com.ginkei.workshopmongo.domain.User;
 import com.ginkei.workshopmongo.dto.AuthorDTO;
+import com.ginkei.workshopmongo.dto.CommentDTO;
 import com.ginkei.workshopmongo.repositories.PostRepository;
 import com.ginkei.workshopmongo.repositories.UserRepository;
 
@@ -36,7 +37,14 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(List.of(maria, alex, bob));
 		
 		var post1 = new Post(null, Instant.parse("2019-03-21T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		var post2 = new Post(null, Instant.parse("2019-03-23T21:13:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		var post2 = new Post(null, Instant.parse("2019-03-23T07:13:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		var c1 = new CommentDTO("Boa Viagem manos!", Instant.parse("2019-03-21T20:13:07Z"), new AuthorDTO(alex));
+		var c2 = new CommentDTO("Aproveite!", Instant.parse("2019-03-22T08:13:07Z"), new AuthorDTO(bob));
+		var c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2019-03-23T08:13:07Z"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(List.of(c1, c2));
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(List.of(post1, post2));
 		
--- src/main/java/com/ginkei/workshopmongo/domain/Post.java
+++ src/main/java/com/ginkei/workshopmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.ginkei.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ginkei.workshopmongo.dto.AuthorDTO;
+import com.ginkei.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -73,6 +78,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -99,7 +113,6 @@ public class Post implements Serializable {
 	}
 
 
-	
 	
 	
 }
